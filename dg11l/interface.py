21dfad4f8022c8a4ca1f2c2d0ebfeb4af8aeb2b7
joni <jonatan@jacoti.com>
2025-03-10 11:15:17 +0100

    dg11l/interface.py: implemented send_remote_action method

--- dg11l/interface.py
+++ dg11l/interface.py
@@ -1,5 +1,6 @@
 import broadlink as blk
 import base64
+import features
 
 
 def get_device(ip):
@@ -12,3 +13,23 @@ def send_b64code(device, b64code):
     byte_code = base64.b64decode(b64code.encode('utf-8'))
     return device.send_data(byte_code)
 
+
+def send_remote_action(
+        device: blk.Device,
+        state: str = None,
+        ifeel_temp: int = None,
+        fan_mode: str = None,
+        mode: str = None,
+        temperature: int = None
+):
+    if state is not None:
+        message = features.get_state_message(state)
+    elif ifeel_temp is not None:
+        message = features.get_ifeel_sensor_message(ifeel_temp)
+    elif fan_mode is not None and mode is not None and temperature is not None:
+        message = features.get_operation_mode_message(
+            fan_mode, mode, temperature
+        )
+    else:
+        raise ValueError('Invalid parameters')
+    return device.send_data(message)
