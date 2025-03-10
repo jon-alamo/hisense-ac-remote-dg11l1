7af2e8d54082a8730f84d3a762022e2b778d06e7
joni <jonatan@jacoti.com>
2025-03-10 11:33:00 +0100

    tests/test_features.py: added initial test files

---
+++ tests/test_features.py
@@ -0,0 +1,19 @@
+import unittest
+
+import dg11l.features as features
+
+
+class TestValidation(unittest.TestCase):
+
+    def test_validate_ifeel_in_range(self):
+        for ifeel_temp in range(37):
+            features.validate_parameters(ifeel_temp=20)
+
+    def test_validate_ifeel_negative(self):
+        with self.assertRaises(ValueError):
+            features.validate_parameters(ifeel_temp=-1)
+
+    def test_validate_ifeel_over_range(self):
+        with self.assertRaises(ValueError):
+            features.validate_parameters(ifeel_temp=37)
+
